ac20310bebf28b7793a12bbcec33de142b167c84
franco-villa <t512700120@unitru.edu.pe>
2024-12-23 17:06:39 -0500

    fix: labels dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -53,44 +53,46 @@ export default function DashboardPage() {
   };
 
   return (
-    <main className="flex flex-1">
-      <div className="flex pr-0">
-        <DashboardSidebar
-          activePage={activePage}
-          onPageChange={handlePageChange}
-        />
-        <SidebarTrigger />
-      </div>
-      <div className="flex flex-col flex-1 mb-4 mx-4 sm:mx-6 md:mx-8 lg:mr-10 xl:mr-12 mt-16 gap-2">
-        {activePage === "#docentes" && <ProfessorPage />}
-        {activePage === "#tramites" && <ProcedurePage />}
-        {activePage === "#registros" && <RecordPage />}
-      </div>
+    <>
+      <main className="flex flex-1">
+        <div className="flex pr-0">
+          <DashboardSidebar
+            activePage={activePage}
+            onPageChange={handlePageChange}
+          />
+          <SidebarTrigger />
+        </div>
+        <div className="flex flex-col flex-1 mb-4 mx-4 sm:mx-6 md:mx-8 lg:mr-10 xl:mr-12 mt-16 gap-2">
+          {activePage === "#docentes" && <ProfessorPage />}
+          {activePage === "#tramites" && <ProcedurePage />}
+          {activePage === "#registros" && <RecordPage />}
+        </div>
 
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div className="flex fixed top-5 right-5 bg-foreground shadow-lg z-50 rounded-full py-2 px-5 w-auto items-center cursor-pointer">
-            <CircleUserRound className="pr-2 w-8 h-8" />
-            <span className="font-semibold hidden md:block">Secretaría</span>
-            <Menu className="block md:hidden text-primary" />
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="p-2">
-          <Link href="#cuenta" onClick={() => setActivePage("#cuenta")}>
-            <DropdownMenuItem className="cursor-pointer">
-              <Settings />
-              Configuración
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="flex fixed top-5 right-5 bg-foreground shadow-lg z-50 rounded-full py-2 px-5 w-auto items-center cursor-pointer">
+              <CircleUserRound className="pr-2 w-8 h-8" />
+              <span className="font-semibold hidden md:block">Secretaría</span>
+              <Menu className="block md:hidden text-primary" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-2">
+            <Link href="#cuenta" onClick={() => setActivePage("#cuenta")}>
+              <DropdownMenuItem className="cursor-pointer">
+                <Settings />
+                Configuración
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuItem
+              className="cursor-pointer"
+              onClick={() => signOut()}
+            >
+              <LogOut />
+              Cerrar Sesión
             </DropdownMenuItem>
-          </Link>
-          <DropdownMenuItem
-            className="cursor-pointer"
-            onClick={() => signOut()}
-          >
-            <LogOut />
-            Cerrar Sesión
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </main>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </main>
+    </>
   );
 }
