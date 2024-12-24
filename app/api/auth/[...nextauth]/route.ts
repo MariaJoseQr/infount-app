43cb220be8eda446329fbed4a91eac3008ae369b
franco-villa <t512700120@unitru.edu.pe>
2024-12-23 19:50:28 -0500

    fix: feat/login-v6

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,48 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { db } from "@/lib/db";
-import bcrypt from 'bcrypt'
-
-export const authOptions: AuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password", placeholder: "*****" },
-            },
-            async authorize(credentials) { //, req
-                console.log(credentials)
-
-                const userFound = await db.user.findFirst({
-                    where: {
-                        username: credentials?.email
-                    }
-                })
-
-                if (!userFound) throw new Error('Usuario no encontrado')
-
-                console.log(userFound)
-                if (!credentials || !credentials.password)
-                    throw new Error('Contraseña no válida')
-
-                const matchPassword = await bcrypt.compare(credentials.password, userFound.password)
-
-                if (!matchPassword) throw new Error('Contraseña Incorrecta')
-
-                return {
-                    id: userFound.id.toString(),
-                    name: userFound.username,
-                    email: userFound.email,
-                }
-            },
-        }),
-    ],
-    pages: {
-        signIn: "/login",
-    }
-};
-
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 // Handler para las rutas GET y POST
 const handler = NextAuth(authOptions);
